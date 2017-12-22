757289d75e36f2896f94cde47d1c9f7a2f1592a6
Brecht Baeten <brecht.baeten@enervalis.com>
2017-12-22 20:57:16 +0100

    updated asyncknxd to return after a connection is made

--- examples/asyncknxd.py
+++ examples/asyncknxd.py
@@ -1,6 +1,8 @@
 import knxpy
 import asyncio
 
+
+
 def callback(data):
     message = knxpy.asyncknxd.default_callback(data)
     print(message)
@@ -10,8 +12,10 @@ connection = knxpy.asyncknxd.KNXD(ip='localhost', port=6720, callback=callback)
 
 async def main():
     await connection.connect()
+    print('connected')
 
     
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.create_task(main())
+loop.run_forever()
 
